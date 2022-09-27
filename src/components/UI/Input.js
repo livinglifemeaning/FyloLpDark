783d70390cc5a77f942a6561238e73ff50598ac3
Asha <maymunahrose@gmail.com>
2022-09-27 13:43:11 -0400

    Fixed input type

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,7 +3,7 @@ import classes from "./Input.module.css";
 
 const Input = (props) => {
   const ref = useRef(); 
-  return <input type="textarea" placeholder={props.placeholder} className={classes.input} ref={ref} onChange={() => props.handleChange(ref)}/>;
+  return <input type="text" placeholder={props.placeholder} className={classes.input} ref={ref} onChange={() => props.handleChange(ref)}/>;
 };
 
 export default Input;
